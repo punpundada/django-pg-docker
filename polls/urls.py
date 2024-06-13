f99e3f034f03af1d7d1a6a9d84f12aae155fa3c0
prajwal <prajwal.parashkar@akronsystems.com>
2024-06-13 15:54:34 +0530

    added view for result, details

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,11 @@ from django.urls import path;
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
     path(route="",name="index",view=views.index),
-    path(route="<int:question_id>/",name="details",view=views.details),
-    path(route="<int:question_id>/results",name="result",view=views.result),
+    path(route="<int:question_id>/",name="detail",view=views.details),
+    path(route="<int:question_id>/results",name="results",view=views.result),
     path(route="<int:question_id>/votes",name="votes",view=views.votes),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404
 from . import models
-from django.template import loader
+
 
 def index(req):
     latest_questions = models.Question.objects.order_by("-pub_date")[:5]
@@ -16,15 +16,16 @@ def details(req,question_id):
     except models.Question.DoesNotExist:
         return Http404(f"Question with id {question_id} not found")
     return render(request=req,context={"question":question},template_name="polls/detail.html")
-    # return HttpResponse(f"you are looking at question details with id{question_id}")
 
 
 def result(req,question_id):
-    # question = models.Question.objects.get(id=question_id)
-    # ctx={"question":question}
-    # return HttpResponse(f"you are looking at question result with id{question_id}")
-    question = get_object_or_404(models.Question,pk=question_id);
-    return 
+    try:
+        question = models.Question.objects.get(pk=question_id)
+        choice = models.Choice.objects.order_by("-votes")[:1][0]
+    except:
+        return Http404(f"No Question found with id {question_id}")
+    print(choice,'choices')
+    return render(request=req,context={"choice":choice,"question":question},template_name="polls/result.html")
 
 
 def votes(req,question_id):
